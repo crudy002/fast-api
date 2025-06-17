fc18163643498b57299194a0c63fdca186111645
Christopher Ruediger <christopher.b.ruediger@gmail.com>
2025-06-17 10:46:09 +0000

    Got spring application working and connected to database

---
+++ backend-services/java-rest/src/main/java/com/example/controller/ItemController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import com.example.model.Item;
+import com.example.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/items")
+public class ItemController {
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @GetMapping
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    @PostMapping
+    public Item createItem(@RequestBody Item item) {
+        return itemRepository.save(item);
+    }
+}
